6c467cc6bdd1a2e01b391091f96cce02f4103462
leoiraira <leoiraira@udec.cl>
2021-07-18 05:55:30 -0400

    hasta la clase 3

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const title = document.getElementById('title');
+const title2 = document.querySelector('#title');
+
+const listItems = document.querySelectorAll('ul li');
+const secondItem = document.querySelectorAll('ul li:nth-child(2)');
+title.style.background = 'yellow';
+list = document.getElementById('list');
+
+
+console.log(title);
+console.log(title2);
+console.log(listItems)
+console.log(secondItem);
+
